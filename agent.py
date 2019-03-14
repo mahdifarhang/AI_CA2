e6be8abbab7d81ff2e673ebecb310a685d656156
mahdifarhang <mafarhang76@gmail.com>
2019-03-14 21:18:00 +0330

    making a random agent

--- agent.py
+++ agent.py
@@ -1,11 +1,20 @@
+from minimax import Minimax
+from tree import Tree
 
 
 class Agent:
-    def __init__(self, color, oppopnentColor, time=None):
+    def __init__(self, color, opponentColor, time=None):
         self.color = color
-        self.oppopnentColor = oppopnentColor
+        self.opponentColor = opponentColor
+        self.height = 3
+
 
     # def move(self):
         # todo: you have to implement this agent.
         # ...
         # return from_cell, to_cell
+
+    def move(self, board):
+        gameTree = Tree(board, self.color, self.opponentColor, self.height)
+        from_cell, to_cell = Minimax.calNextMove(gameTree, self.height)
+        return from_cell, to_cell
